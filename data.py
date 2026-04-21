2100ee5cad505326e15fdf5cb6985900cf82a3a6
Jack page <jpage2603@gmail.com>
2026-04-21 10:39:20 +0100

    first commit

---
+++ data.py
@@ -0,0 +1,8 @@
+class payee:
+    def __innit__(self, sort_code, account_num, name, bank):
+        self.name = name
+        self.sort_code = sort_code
+        self.account_num = account_num
+        self.bank = bank 
+
+    
