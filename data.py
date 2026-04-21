6080455864047b44082c09d1d65240695fd30bfb
Jack page <jpage2603@gmail.com>
2026-04-21 10:48:41 +0100

    Initial banking app code

--- data.py
+++
@@ -1,8 +0,0 @@
-class payee:
-    def __innit__(self, sort_code, account_num, name, bank):
-        self.name = name
-        self.sort_code = sort_code
-        self.account_num = account_num
-        self.bank = bank 
-
-    
