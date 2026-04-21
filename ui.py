dda99e36b1e3216e62916847171dd12d91f8e553
Jack page <jpage2603@gmail.com>
2026-04-21 14:54:52 +0100

    added login credetial authenticator

--- ui.py
+++ ui.py
@@ -1,11 +1,21 @@
 from pywebio.input import input, PASSWORD
+from pywebio.output import put_text
 from models import Payee, User
-
-username = input("username")
-password = input("password", type=PASSWORD)
 
 users = []
 
 test_user = User(username="admin", password="password")
 users.append(test_user)
+
+def login():
+    username = input("username")
+    password = input("password", type=PASSWORD)
+
+    for user in users:
+        if user.username == username and user.password == password:
+            put_text("login successful")
+            return user
+    
+    put_text("Wrong Credentials")
+            
 
